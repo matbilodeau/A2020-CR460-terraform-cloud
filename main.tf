bd4f0caa3c367825473d33531e702d7d8cb6db60
matbilodeau <matbilodeau@users.noreply.github.com>
2020-09-08 17:10:54 -0400

    Update main.tf
    to use github workflow

--- main.tf
+++ main.tf
@@ -4,6 +4,17 @@ provider "google" {
   zone    = var.zone
 }
 
+terraform {
+   backend "remote" {
+     # The name of your Terraform Cloud organization.
+     organization = "A2020-CR460"
+     # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "A2020-CR460-terraform-cloud"
+    }
+  }
+}
+
 resource "google_compute_instance" "tfc-exemple" {
   name         = "tfc-exemple"
   machine_type = "f1-micro"
